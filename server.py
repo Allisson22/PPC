f95101f0197f069c724740f1b7929b6d82e89122
Roussel Allisson <aroussel@tc405-114-14.insa-lyon.fr>
2024-01-23 09:50:53 +0100

    (server) : initialisation des dicos

--- server.py
+++ server.py
@@ -1,12 +1,53 @@
-import sysv_ipc
-import time
+from random import *
 
-key = 128
+nb_joueurs = 2
 
-que = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+couleurs = ["bleu", "rouge", "vert", "noir", "jaune", "orange"]
 
-on = True
-while on :
-    mess,_ = que.receive()
-    print(mess.decode())
-    time.sleep(5)
+def deck(nb_joueurs):
+    liste_cartes = []
+    for i in range (nb_joueurs) :
+        for un in range (3):
+            liste_cartes.append({couleurs[i], 1})
+        for multiples in range (2):
+            for nombre in [2,3,4] :
+                liste_cartes.append({couleurs[i],nombre})
+        liste_cartes.append({couleurs[i],5})
+    return liste_cartes
+
+
+def hand(nb_joueurs, liste_cartes):
+    dico_hand = {}
+    for i in range(nb_joueurs):
+        dico_hand[i] = []
+        for compteur in range (5):
+            index = randint(1, len(liste_cartes)-1)
+            carte_aleatoire = liste_cartes[index]
+            dico_hand[i].append(carte_aleatoire)
+            liste_cartes.pop(index)
+    return dico_hand
+
+
+def information_token(nb_joueurs):
+    nb_token = 3*nb_joueurs + 3
+    return nb_token
+
+
+def fuze_token():
+    nb_token = 3
+    return nb_token
+
+
+def suite():
+    dico_suite = {}
+    for i in range (5):
+        dico_suite[i] = [True,False,False,False,False,False]
+    return dico_suite
+
+gros_dico = {}
+gros_dico["deck"] = deck(nb_joueurs)
+gros_dico["hand"] = hand(2,deck(2))
+gros_dico["suite"] = suite()
+gros_dico["information_token"] = information_token(nb_joueurs)
+gros_dico["fuze_token"] = fuze_token()
+print(gros_dico)
