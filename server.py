6572da963ba6516161a8ef6ed07c771aa79ce759
Roussel Allisson <aroussel@tc405-114-14.insa-lyon.fr>
2024-01-23 10:32:21 +0100

    (server) : initialisation des dicos + debut server

--- server.py
+++ server.py
@@ -1,6 +1,10 @@
 from random import *
+from multiprocessing import Process, Manager
+import socket
 
 nb_joueurs = 2
+
+#####INITIALISATION DICOS#####
 
 couleurs = ["bleu", "rouge", "vert", "noir", "jaune", "orange"]
 
@@ -44,10 +48,60 @@ def suite():
         dico_suite[i] = [True,False,False,False,False,False]
     return dico_suite
 
-gros_dico = {}
-gros_dico["deck"] = deck(nb_joueurs)
-gros_dico["hand"] = hand(2,deck(2))
-gros_dico["suite"] = suite()
-gros_dico["information_token"] = information_token(nb_joueurs)
-gros_dico["fuze_token"] = fuze_token()
-print(gros_dico)
+#####COMMUNICATION######
+
+HOST = "localhost"
+PORT = 6666
+
+def client_handler(s, a):
+    with s:
+        print("Connected to client: ", a)
+        data = s.recv(1024)
+        while len(data):
+            s.sendall(data)
+            data = s.recv(1024)
+        print("Disconnecting from client: ", a)
+            
+
+
+######MAIN######
+
+if __name__ == '__main__':
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)
+        client_socket, address = server_socket.accept()
+        with client_socket:
+            print("Connected to client: ", address)
+            data = client_socket.recv(1024)
+            while len(data):
+                client_socket.sendall(data)
+                data = client_socket.recv(1024)
+            p = Process(target=client_handler, args=(client_socket, address))
+            p.start()
+             
+        for i in range (nb_joueurs-1):
+            client_socket, address = server_socket.accept()
+            p = Process(target=client_handler, args=(client_socket, address))
+            p.start()
+
+
+
+
+
+
+    with Manager() as manager:
+
+        gros_dico = manager.dict()
+        gros_dico = {}
+        gros_dico["deck"] = deck(nb_joueurs)
+        gros_dico["hand"] = hand(2,deck(2))
+        gros_dico["suite"] = suite()
+        gros_dico["information_token"] = information_token(nb_joueurs)
+        gros_dico["fuze_token"] = fuze_token()
+        
+        p = Process(target=Processplayer, args=(gros_dico))
+        p.start()
+
+
+
