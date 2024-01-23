7f07d8b2724b9cdceb05f8d8417f99e8dced8019
Hautbois Guewen <ghautbois@tc405-114-10.insa-lyon.fr>
2024-01-23 10:01:43 +0100

    (socket_player): connection par socket fonctionelle avec reponse du client modulable

--- client.py
+++ client.py
@@ -1,16 +1,19 @@
 import socket
 
 HOST = "localhost"
-PORT = 1790
+PORT = 1798
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
         client_socket.connect((HOST, PORT))
         on = True
         while on :
-            mess = client_socket.recv(1024).decode().split()
-            print(mess[1])
-            if int(mess[0]) == 1 :
+            mess = client_socket.recv(1024).decode()
+            val, message = mess[0], mess[2:]
+            print(val,message)
+            if int(val) == 1 :
                 rep = input("réponse> ")
                 client_socket.sendall(rep.encode())
+            else :
+                client_socket.sendall(''.encode())
             
 
--- process_player.py
+++ process_player.py
@@ -1,12 +1,39 @@
 import sysv_ipc
-import time
+import socket
+from multiprocessing import Process, Manager
 
-key = 128
 
-que = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
 
-on = True
-while on :
-    l = "thomzs bleu\n prout"
-    que.send(l.encode())
-    time.sleep(5)
+
+def player_main(key,data,socket_player) :
+    que = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+    on = True
+    while on :
+        print(2,data["hand"])
+        mess = f"0 {data[f'hand']}"
+        socket_player.sendall(mess.encode())
+        _ = socket_player.recv(1024)
+        mess = "1 Nombre de joueurs"
+        socket_player.sendall(mess.encode())
+        mess = client_socket.recv(1024).decode()
+        print(mess)
+
+
+
+
+if __name__ == "__main__" :
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
+        HOST = "localhost"
+        PORT = 1798
+        key = 128
+
+        server_socket.bind((HOST, PORT))
+        server_socket.listen(1)
+        client_socket, address = server_socket.accept()
+        with Manager() as manager :
+            data = manager.dict()
+            data["hand"] = [("bleu",1),("rouge",5),("bleu",2),("jaune",2),("jaune",1)]
+            print(1,data["hand"])
+            player = Process(target=player_main, args=(key,data,client_socket))
+            player.start()
+            player.join()
