834ef8bc9eb422eff8264e4dbcc2ef8c0a3fd0a3
Allisson22 <allisson.roussel@insa-lyon.fr>
2024-01-23 15:45:07 +0100

    gestion plusieurs clients

--- server.py
+++ server.py
@@ -3,6 +3,7 @@ from multiprocessing import Process, Manager
 import socket
 import sysv_ipc
 import signal
+import time
 import os
 #####INITIALISATION DICOS#####
 
@@ -63,13 +64,14 @@ def message_client(socket_player,message,retour = "Nothing"):
     return reponse
 
 
-def player_main(key,data,socket_player) :
-    que = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
-    on = True
-    while on :
-        print(2,data["hand"])
-        print(message_client(socket_player,f"0 {data['hand']}"))
-        print(message_client(socket_player,"1 Nombre de joueurs"))
+def player_main(data, socket_player):
+    key = data.get('key')  # Use data.get('key') to avoid KeyError
+    if key is not None:
+        que = sysv_ipc.MessageQueue(key, sysv_ipc.IPC_CREAT)
+        on = True
+        while on:
+            print(2, data["hand"])
+            time.sleep(10000)
             
 def stop_handler(signum, frame):
     print("Received signal to stop the game.")
@@ -80,47 +82,46 @@ def stop_handler(signum, frame):
 
 ######MAIN######
 
+
 if __name__ == '__main__':
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as server_socket:
         HOST = "localhost"
-        PORT = 6666
+        PORT = 6688
         key = 128
+        nb_joueurs = 0
 
         server_socket.bind((HOST, PORT))
         server_socket.listen(1)
         client_socket, address = server_socket.accept()
-        with client_socket:
-            print("Connected to client: ", address)
-            while nb_joueurs < 2 :
-                nb_joueurs = message_client(client_socket,"1 Nombre de joueurs", "int")
-            with Manager() as manager:
+        print("Connected to client: ", address)
+        while nb_joueurs < 2:
+            nb_joueurs = message_client(client_socket, "1 Nombre de joueurs", "int")
+        with Manager() as manager:
+            while True :
                 gros_dico = manager.dict()
-                gros_dico = {}
                 gros_dico["deck"] = deck(nb_joueurs)
-                gros_dico["hand"] = hand(nb_joueurs,deck(nb_joueurs))
+                gros_dico["hand"] = hand(nb_joueurs, deck(nb_joueurs))
                 gros_dico["suite"] = suite()
                 gros_dico["information_token"] = information_token(nb_joueurs)
                 gros_dico["fuze_token"] = fuze_token()
                 gros_dico["turn"] = 0
 
-                p = Process(target=player_main, args=(os.getpid(), key, gros_dico, client_socket, signal.SIGTERM, 0))
+                # Add the 'key' to the gros_dico
+                gros_dico['key'] = key
+
+                # Pass the manager object and player_id directly to the target function
+                p = Process(target=player_main, args=(gros_dico, client_socket))
                 p.start()
                 child_processes.append(p.pid)
+                message_client(client_socket, "0 Vous êtes le joueur 0")
 
                 for i in range(nb_joueurs - 1):
                     client_socket, address = server_socket.accept()
-                    p = Process(target=player_main, args=(os.getpid(), key, gros_dico, client_socket, signal.SIGTERM, i + 1))
+                    p = Process(target=player_main, args=(gros_dico, client_socket))
                     p.start()
                     child_processes.append(p.pid)
+                    message_client(client_socket, f"0 Vous êtes le joueur {i+1}")
 
                 gros_dico["turn"] = 1
-
-
-
-                
-
-
-
-
-
+                p.join()
 
